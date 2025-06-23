70fcdc198e027ac8d83bd953e718e0a69f5192cd
Sajal Sisodia <sajal89304@gmail.com>
2025-06-23 10:04:05 +0530

    add loader and use effect and loder state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,33 +1,63 @@
-import Post from "./Post"
+import Post from "./Post";
 
-import {PostList as PostListData} from '../strore/posts-list-store'
-import { useContext } from "react"
-import WelcomeMessage from './WelcomeMessage'
-
+import { PostList as PostListData } from "../strore/posts-list-store";
+import { useContext, useEffect, useState } from "react";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addIntialsPosts } = useContext(PostListData);
- 
-  const handelOnClickPosts = () => {
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then(
-        data => {
-          addIntialsPosts(data.posts)
-        }
-      );
+  const [fetching, setfetchig] = useState(false);
+  // const [dataFetched , setdataFateched] = useState();
 
-  } 
- 
+  useEffect(() => {
+    setfetchig(true);
+    const controller = new AbortController();
+    const signal = controller.signal
+    fetch("https://dummyjson.com/posts", {signal})
+      .then((res) => res.json())
+      .then((data) => {
+        addIntialsPosts(data.posts);
+        setfetchig(false);
+      });
+
+    return () => {
+      console.log("clen up ");
+      controller.abort();
+
+
+    }
+  }, []);
+
+  // if (! dataFetched){
+  //     fetch("https://dummyjson.com/posts")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         addIntialsPosts(data.posts);
+  //       });
+  //       setdataFateched(true);
+
+  // }
+
+  // const handelOnClickPosts = () => {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then(
+  //       data => {
+  //         addIntialsPosts(data.posts)
+  //       }
+  //     );
+
+  // }
+
   return (
     <>
-      {postList.length === 0 && (<WelcomeMessage onGetPostsClick= {handelOnClickPosts} />)}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
 
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
-}
+};
 
 export default PostList;
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -4,9 +4,10 @@ const WelcomeMessage  = ({onGetPostsClick}) => {
   return (
     <center>
       <h1> There is no post yet</h1>
-      <button type="button" className="btn btn-primary" onClick={onGetPostsClick}>
+      
+      {/* <button type="button" className="btn btn-primary" onClick={onGetPostsClick}>
         Get Posts
-      </button>
+      </button> */}
     </center>
   );
 }
